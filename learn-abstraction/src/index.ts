c87d64273d56a43514957de864faa448c9b6d48d
Wiwie Sanjaya <wiwiewei18@gmail.com>
2024-06-21 09:00:52 +0700

    refactor greet method in user class

--- learn-abstraction/src/index.ts
+++ learn-abstraction/src/index.ts
@@ -1,81 +1,22 @@
-export class User {
+import { Gender } from "./enum/gender";
+import { Greeter } from "./greeter";
+
+export class User extends Greeter {
   name: string;
+  gender?: Gender;
   static savedUsers: User[];
   friends: User[];
 
   constructor(name: string, savedUsers?: User[]) {
+    super();
+
     this.name = name;
     User.savedUsers = savedUsers || [];
     this.friends = [];
   }
 
-  greet(
-    name?: string,
-    isMale?: boolean,
-    isFemale?: boolean,
-    isMorning?: boolean,
-    isAfternoon?: boolean,
-    isEvening?: boolean,
-    isNight?: boolean,
-    lang?: 'indo'
-  ) {
-    if (lang === 'indo') {
-      if (isMorning) {
-        if (isFemale) {
-          return `Selamat pagi Bu ${name}`;
-        }
-
-        if (isMale) {
-          return `Selamat pagi Pak ${name}`;
-        }
-
-        return `Selamat pagi ${name}`;
-      }
-
-      if (isMale) {
-        return `Halo Pak ${name}`;
-      }
-
-      if (isFemale) {
-        return `Halo Bu ${name}`;
-      }
-
-      return `Halo ${name}`;
-    }
-
-    if (isMorning) {
-      if (isMale && isFemale) {
-        return `Good morning ${name}`;
-      }
-
-      if (isFemale) {
-        return `Good morning Ms ${name}`;
-      }
-
-      return `Good morning ${name}`;
-    }
-
-    if (isAfternoon) {
-      return `good afternoon ${name}`;
-    }
-
-    if (isEvening) {
-      return `good evening ${name}`;
-    }
-
-    if (isNight) {
-      return `good night ${name}`;
-    }
-
-    if (isMale) {
-      return `wassup Mr ${name}`;
-    }
-
-    if (isFemale) {
-      return `wassup Ms ${name}`;
-    }
-
-    return `wassup ${name}`;
+  setGender(gender: Gender) {
+    this.gender = gender;
   }
 
   static saveToDatabase(user: User) {
