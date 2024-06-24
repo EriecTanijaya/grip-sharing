7e8c199118d74e3cc92264a01d8397b64b60b462
Eriec Tanijaya <eriectan88@gmail.com>
2024-06-24 23:03:39 +0700

    refactor: remove dead codes

--- learn-abstraction/src/index.ts
+++ learn-abstraction/src/index.ts
@@ -1,11 +1,9 @@
 export class User {
   name: string;
-  static savedUsers: User[];
   friends: User[];
 
   constructor(name: string, savedUsers?: User[]) {
     this.name = name;
-    User.savedUsers = savedUsers || [];
     this.friends = [];
   }
 
@@ -76,14 +74,6 @@ export class User {
     }
 
     return `wassup ${name}`;
-  }
-
-  static saveToDatabase(user: User) {
-    this.savedUsers?.push(user);
-  }
-
-  static getUser(name: string) {
-    return User.savedUsers?.find((u) => u.name === name);
   }
 
   addFriend(user: User) {
